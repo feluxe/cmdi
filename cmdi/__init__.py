c44ec31f6fd61baaa4d6218c64b799a9100fd9e4
feluxe <felix@meyerwolters.de>
2019-08-02 19:00:17 +0200

    Add missing exports to __init__.py

--- cmdi/__init__.py
+++ cmdi/__init__.py
@@ -1,2 +1,2 @@
-from .lib import CmdResult, strip_cmdargs, print_summary
+from .lib import CmdResult, strip_cmdargs, print_summary, run_subprocess, resolve_popen, read_popen_pipes, POPEN_DEFAULTS
 from .decorator import command, Pipe, STDOUT
