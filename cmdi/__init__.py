12323596442e90e98ce6784cba05c6612ce11eb0
feluxe <felix@meyerwolters.de>
2019-08-06 17:59:06 +0200

    Fix issue with exports.

--- cmdi/__init__.py
+++ cmdi/__init__.py
@@ -1,2 +1,2 @@
-from .lib import CmdResult, strip_cmdargs, print_summary, run_subprocess, resolve_popen, read_popen_pipes, POPEN_DEFAULTS
+from .lib import *
 from .decorator import command, Pipe, STDOUT
